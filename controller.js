3e59995f071119102f2cce9e8182619d1207c5a8
bloodykheeng <bloodykheeng@gmail.com>
2021-07-18 10:54:04 +0300

    34

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ const { v4 : uuidv4} = require("uuid")
  const connection = 
   mysql.createConnection({
     
-host  : process.env.MYSQL_SERVICE_HOST,
+host  : process.env.HOST,
   user     : process.env.USER,
   password : process.env.PASSWORD,
   database : process.env.DATABASE
