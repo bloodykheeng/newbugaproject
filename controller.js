050908c3a9f9b1d14f5f87f176a8c0b944af6ce6
bloodykheeng <bloodykheeng@gmail.com>
2021-07-21 12:59:55 +0300

    bloody

--- controller.js
+++ controller.js
@@ -4,19 +4,19 @@
 const { v4 : uuidv4} = require("uuid")
  const connection = 
   mysql.createConnection({
-    
+    /*
   host : "localhost",
   user     : "root",
   password : "",
   database : "bugatech"
-  
+  */
 
-/*
+
 host : "172.30.72.177",
   user     : "root",
   password : "1234",
   database : "bugatech"
-*/
+
   
 })
 
